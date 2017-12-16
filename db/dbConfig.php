9f3f2ba97f51a0b427ee9a741235663527ee4cd4
naraa <j.it13d162@soc.edu.mn>
2017-12-16 17:39:00 +0800

    added book service

--- db/dbConfig.php
+++ db/dbConfig.php
@@ -3,4 +3,17 @@
 $connect = mysql_connect("localhost","root","") or die('Database Not Connected. Please Fix the Issue! ' . mysql_error());
 mysql_select_db("onlinebookstore", $connect);
 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "onlinebookstore";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
 ?>
---
+++ services/book.php
@@ -0,0 +1,31 @@
+<?php
+	include_once "../db/dbConfig.php";
+
+
+	include_once "../db/dbConfig.php";
+	if(isset($_GET['type'])){
+		switch ($_GET['type']) {
+			case 'getAllBooks':
+			    $sql = "SELECT * FROM book";
+				$result = $conn->query($sql);
+
+				if ($result->num_rows > 0) {
+				    // output data of each row
+				    while($row = $result->fetch_assoc()) {
+				        $new_array[] = $row;
+				    }
+				    echo json_encode($new_array);
+				} else {
+				    echo "0 results";
+				}
+				
+				# code...
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+		$conn->close();
+	}
+?>
